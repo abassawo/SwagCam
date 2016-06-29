96c2db036e0220d4a41eb6fd47f761a6b7f587cd
Abass Bayo-Awoyemi <Abass@C4Qs-MacBook-Pro.local>
2016-06-29 03:10:40 -0400

    custom camera view and fragment added

--- app/src/main/java/abassawo/c4q/nyc/swagcam/MainActivity.java
+++ app/src/main/java/abassawo/c4q/nyc/swagcam/MainActivity.java
@@ -1,28 +1,29 @@
 package abassawo.c4q.nyc.swagcam;
 
-
-import android.app.Activity;
-import android.location.LocationManager;
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.support.v4.app.FragmentManager;
+import android.app.FragmentManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        FragmentManager fm = getSupportFragmentManager();
-        fm.beginTransaction().add(R.id.main_container, CameraFragment.getInstance()).commit();
+        setContentView(R.layout.activity_camera);
+        FragmentManager fm = getFragmentManager();
+        fm.beginTransaction().replace(R.id.container, SwagCamFragment.newInstance()).commit();
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(getResources().getString(R.string.app_name));
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
     }
+
 }
 
 
